c1eb251476ecede18737e742925d8df50a7303e4
TEC <git@tecosaur.net>
2023-03-01 00:42:03 +0800

    ox-html: Update LaTeX export for images and mathml
    * lisp/ox-html.el (org-html-latex-environment, org-html-format-latex,
    org-latex-to-html-convert-command, org-format-latex-as-html,
    org-html-latex-image-options): Introduce new functions and variables to
    work directly with the new org-latex-preview and ox-mathml API.
    
    * lisp/org-latex-preview.el (org-format-latex-as-html,
    org-latex-preview--tex-styled, org-latex-preview-replace-fragments,
    org-latex-to-html-convert-command, org-latex-preview-options): Move all
    of the HTML-specific processing out of org-latex-preview.el, and into
    ox-html.el, and remove now-obsolete code.
    
    * lisp/org-compat.el (org-create-formula-image): Relocate a copy of
    `org-create-formula-image' here, and mark it as obsolete.

--- lisp/org-compat.el
+++ lisp/org-compat.el
@@ -936,8 +936,49 @@ a HTML file."
 	  (delete-file (concat texfilebase e))))
       image-output-file)))
 
+;; FIXME: Unused; obsoleted; to be removed.
+(defun org-html-format-latex (latex-frag processing-type info)
+  "Format a LaTeX fragment LATEX-FRAG into HTML.
+PROCESSING-TYPE designates the tool used for conversion.  It can
+be `mathjax', `verbatim', `html', nil, t or symbols in
+`org-preview-latex-process-alist', e.g., `dvipng', `dvisvgm' or
+`imagemagick'.  See `org-html-with-latex' for more information.
+INFO is a plist containing export properties."
+  (let ((cache-relpath "") (cache-dir ""))
+    (unless (or (eq processing-type 'mathjax)
+                (eq processing-type 'html))
+      (let ((bfn (or (buffer-file-name)
+		     (make-temp-name
+		      (expand-file-name "latex" temporary-file-directory))))
+	    (latex-header
+	     (let ((header (plist-get info :latex-header)))
+	       (and header
+		    (concat (mapconcat
+			     (lambda (line) (concat "#+LATEX_HEADER: " line))
+			     (org-split-string header "\n")
+			     "\n")
+			    "\n")))))
+	(setq cache-relpath
+	      (concat (file-name-as-directory org-preview-latex-image-directory)
+		      (file-name-sans-extension
+		       (file-name-nondirectory bfn)))
+	      cache-dir (file-name-directory bfn))
+	;; Re-create LaTeX environment from original buffer in
+	;; temporary buffer so that dvipng/imagemagick can properly
+	;; turn the fragment into an image.
+	(setq latex-frag (concat latex-header latex-frag))))
+    (org-export-with-buffer-copy
+     :to-buffer (get-buffer-create " *Org HTML Export LaTeX*")
+     :drop-visibility t :drop-narrowing t :drop-contents t
+     (erase-buffer)
+     (insert latex-frag)
+     (org-format-latex cache-relpath nil nil cache-dir nil
+		       "Creating LaTeX Image..." nil processing-type)
+     (buffer-string))))
+
 (make-obsolete #'org-format-latex "to be removed" "9.7")
 (make-obsolete #'org-create-formula-image "to be removed" "9.7")
+(make-obsolete #'org-html-format-latex "to be removed" "9.7")
 
 ;; FIXME: Unused; obsoleted; to be removed.
 (defun org-let (list &rest body) ;FIXME: So many kittens are suffering here.
